e33d286637bcca934d08af14a9924a757c9e5670
JoKircher <79573182+JoKircher@users.noreply.github.com>
2021-12-08 13:25:26 +0100

    Update crosscheckscript.jl

--- crosscheckscript.jl
+++ crosscheckscript.jl
@@ -16,9 +16,9 @@ errorsCATIA = [[0.000005, 0.02, 0.0003], [0.005, 0.06, 1], [0.04, 0.4, 0.2, 0.2,
 # MixtureGases, ControlledTemperature, CoupledClutches
 # [der_p, der_T], [heatCapacitor_T, switch_Controll], [J1_w,J2_w,J3_w,J4_w]
 errorsDymola = [[0.000005, 0.015, 0.0003], [0.005, 0.02, 1], [0.09, 0.4, 0.03, 0.03, 0.08]]
-# BouncingBalls_sf, TriggeredSubsystems_sf, TestModel1_sf
+# BouncingBalls_sf, TriggeredSubsystems_sf, TestModel1_sf, TestModel2_sf
 # [Ball1_pos,Ball2_pos,Ball1_vel,Ball2_vel], [yRising,yFalling,yEither], [Noise,Noise_Filt,Noise_Filt_Sat,Sine,Sine_Filt,Sine_Filt_Quantized]
-errorsDS = [[0.000005, 0.0005, 0.0005, 0.0005, 0.0005], [0.000005, 5e-6, 5e-6, 5e-6], [0.000005, 9, 3.1, 1, 0.00005, 0.00005, 0.11], [0.02, 0.5, 0.5]]
+errorsDS = [[0.000005, 0.0005, 0.0005, 0.0005, 0.0005], [0.000005, 5e-6, 5e-6, 5e-6], [0.000005, 9, 3.1, 1, 0.00005, 0.00005, 0.11], [0.02, 1, 3]]
 # Continuous, IntegrateSignal, EmbeddedCode
 # [Out1,Out2[1],Out2[2],Out3,Out4,Out5], [Out1], [Out1]
 errorsFMIToolbox = [[0.000005, 1e-20, 1e-20, 1e-20, 1e-15, 1e-16, 1e-14], [0.02, 0.008], [0.000005, 1e-15]]
@@ -82,11 +82,11 @@ for type in modeltype
                 end
             else
                 if type == "cs"
-                    data = fmiSimulate(fmu,t_start, t_stop;recordValues=name[2:end], saveat=rowsres.time, setup=true)
+                    success, data = fmiSimulate(fmu,t_start, t_stop;recordValues=name[2:end], saveat=rowsres.time, setup=true)
                     df = DataFrame()
-                    df.time = fmi2SimulationResultGetTime(data)
+                    df.time = data.t
                     for i in 2:length(name)
-                        insertcols!(df, i, Symbol(name[i]) => fmi2SimulationResultGetValues(data, name[i]))
+                        insertcols!(df, i, Symbol(name[i]) => [x[i-1] for x in data.saveval])
                     end
                 else
                     @warn "Noch nicht implementiert"
